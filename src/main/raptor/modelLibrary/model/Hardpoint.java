d9bf6d2b7d856de9e46c6a2ef8027c47492a64f4
raptor0719 <shortman_gamer@hotmail.com>
2019-02-08 09:58:27 -0500

    Add a reference point to the hardpoints

--- src/main/raptor/modelLibrary/model/Hardpoint.java
+++ src/main/raptor/modelLibrary/model/Hardpoint.java
@@ -1,24 +1,37 @@
 package raptor.modelLibrary.model;
 
+import raptor.modelLibrary.model.point.IPointReader;
+import raptor.modelLibrary.model.point.Point;
+
 public class Hardpoint implements IHardpointPosition {
 	private int x;
 	private int y;
 	private int rotation;
 
+	private final IPointReader referencePoint;
+
 	public Hardpoint(final int initX, final int initY, final int initRotation) {
 		x = initX;
 		y = initY;
 		rotation = initRotation;
+		this.referencePoint = new Point(0, 0);
+	}
+
+	public Hardpoint(final int initX, final int initY, final int initRotation, final IPointReader referencePoint) {
+		x = initX;
+		y = initY;
+		rotation = initRotation;
+		this.referencePoint = (referencePoint != null) ? referencePoint : new Point(0, 0);
 	}
 
 	@Override
 	public int getX() {
-		return x;
+		return referencePoint.getX() + x;
 	}
 
 	@Override
 	public int getY() {
-		return y;
+		return referencePoint.getY() + y;
 	}
 
 	@Override
